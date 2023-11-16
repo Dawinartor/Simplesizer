d87cee98db425c1dacbc364b97fda75cccb3674a
David Melamed <david.melamed@web.de>
2023-11-16 16:14:39 +0100

    Implement functionalities with pygame and threads

--- prototyp/simplesizer.py
+++ prototyp/simplesizer.py
@@ -1,18 +1,26 @@
 import sys
 import threading
 from pydub import AudioSegment
-from pydub.playback import play
+import pygame
 
 class SoundPlayer:
     def __init__(self, file_path):
         self.sound = AudioSegment.from_file(file_path)
-        self.playing_thread = None
         self.paused = False
         self.stopped = False
 
     def play(self):
-        self.playing_thread = threading.Thread(target=self._play_sound)
-        self.playing_thread.start()
+        pygame.mixer.init()
+        pygame.mixer.music.load(file_path)
+        pygame.mixer.music.play()
+
+        while pygame.mixer.music.get_busy() and not self.stopped:
+            pygame.time.Clock().tick(10)
+            if self.paused:
+                pygame.mixer.music.pause()
+                while self.paused:
+                    pass
+                pygame.mixer.music.unpause()
 
     def pause(self):
         self.paused = True
@@ -22,19 +30,10 @@ class SoundPlayer:
 
     def stop(self):
         self.stopped = True
+        pygame.mixer.music.stop()
 
-    def _play_sound(self):
-        play(self.sound, stop_func=self._check_stop)
-
-    def _check_stop(self):
-        return self.stopped
-
-def play_sound(file_path):
-    player = SoundPlayer(file_path)
-    print(f"Playing sound: {file_path}")
-    player.play()
-
-    while True:
+def user_input(player):
+    while not player.stopped:
         command = input("Enter 'p' to pause, 'r' to resume, 's' to stop, or 'q' to quit: ").lower()
 
         if command == 'p':
@@ -46,11 +45,9 @@ def play_sound(file_path):
         elif command == 's':
             player.stop()
             print("Stopped")
-            break
         elif command == 'q':
             player.stop()
             print("Exiting")
-            break
         else:
             print("Invalid command")
 
@@ -59,5 +56,15 @@ if __name__ == "__main__":
         print("Usage: python script_name.py <sound_file_path>")
         sys.exit(1)
 
-    sound_file_path = sys.argv[1]
-    play_sound(sound_file_path)
+    file_path = sys.argv[1]
+    player = SoundPlayer(file_path)
+
+    # Start a separate thread for user input
+    input_thread = threading.Thread(target=user_input, args=(player,))
+    input_thread.start()
+
+    # Start audio playback in the main thread
+    player.play()
+
+    # Wait for the input thread to finish
+    input_thread.join()
