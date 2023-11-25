d730291243ce88663006ed12238a4d47278f4b82
melamed <melameddavid19@gmail.com>
2023-11-25 21:43:47 +0100

    Renamed based on best-practice

--- prototyp/simplesizerTwo.py
+++ prototyp/simplesizerTwo.py
@@ -4,16 +4,17 @@ import pygame
 from pydub import AudioSegment
 from pydub.playback import play
 
-class SoundPlayer:
-    def __init__(self, file_path):
-        self.sound = file_path
-        self.frequency = 22050 #22050
+
+class Simplesizer:
+    def __init__(self, song_path):
+        self.song = song_path
         self.speed = 1.0
 
+    def load_player(self):
+        pygame.mixer.init()
+        pygame.mixer.music.load(self.song)
 
     def play(self):
-        pygame.mixer.init()
-        pygame.mixer.music.load(file_path)
         pygame.mixer.music.play()
 
     def pause(self):
@@ -39,66 +40,57 @@ class SoundPlayer:
             pygame.mixer.music.set_volume(musicVolume)
 
     def playback(self, speed):
+        pygame.quit()
         self.speed = float(speed)
         audio = AudioSegment.from_file(self.sound, format="mp3")
-        increasedSpeedAudio = audio.speedup(playback_speed=self.speed)
-        play(increasedSpeedAudio)
-
-
-
-
-
-
-
-
+        increased_speed_audio = audio.speedup(playback_speed=self.speed)
+        play(increased_speed_audio)
 
 
 def user_input(player):
     while True:
-        userInput = input("Enter 'a' to pause, 's' to resume, 'd' to stop, 'w' to adjust song volume, 'e' to adjust song speed or 'q' to quit: ").lower()
-        inputValues = userInput.split()
-        menuChoice = inputValues[0]
+        user_input = input(
+            "Enter 'a' to pause, 's' to resume, 'd' to stop, 'w' to adjust song volume, 'e' to adjust song speed or 'q' to quit: ").lower()
+        input_values = user_input.split()
+        menu_choice = input_values[0]
 
 
-        if len(inputValues) == 1:
+        if len(input_values) == 1:
 
-            if menuChoice == 'a':  # pause the music
+            if menu_choice == 'a':  # pause the music
                 player.pause()
                 print("Paused")
-            elif menuChoice == 's':  # resume the music
+            elif menu_choice == 's':  # resume the music
                 player.resume()
                 print("Resumed")
-            elif menuChoice == 'd':  # stop the music BUT do we need this function? And if yes, what for?
+            elif menu_choice == 'd':  # stop the music BUT do we need this function? And if yes, what for?
                 player.stop()
                 print("Stopped")
-            elif menuChoice == 'q':  # quit the program
+            elif menu_choice == 'q':  # quit the program
                 # this order is necessary!
                 print("Exiting")
                 player.quit()
             else:
                 print("Wrong button bruh...")
 
+        if len(input_values) == 2:
+            aditional_menu_value = input_values[1]
 
-        if len(inputValues) == 2:
-            aditionalMenuValue = inputValues[1]
+            if menu_choice == 'w':  # adjust song volume
+                player.pitch(aditional_menu_value)
+            elif menu_choice == 'e':  # adjust song speed
+                player.playback(aditional_menu_value)
 
-            if menuChoice == 'w': # adjust song volume
-                player.pitch(aditionalMenuValue)
-            elif menuChoice == 'e': # adjust song speed
-                player.playback(aditionalMenuValue)
-
-
-
-
-
+# starting the core functionalities
 if __name__ == "__main__":
+    print(sys.argv)
     if len(sys.argv) != 2:
         print("")
         print("Usage: python script_name.py <sound_file_path>")
         sys.exit(1)
 
-    file_path = sys.argv[1]
-    player = SoundPlayer(file_path)
+    simplesizer = Simplesizer(sys.argv[1])
+    simplesizer.load_player()
 
     # Start a separate thread for user input
     input_thread = threading.Thread(target=user_input, args=(player,))
